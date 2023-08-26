3566df498aa97de95d187c3c893d66ce06d75de8
Will Brinkman <willbkman@gmail.com>
2023-08-25 23:03:23 -0500

    Added utility to append items to the .

---
+++ script.js
@@ -0,0 +1,12 @@
+function addItem() {
+    const itemInput = document.getElementById('itemInput');
+    const itemList = document.getElementById('itemList');
+
+    if (itemInput.value.trim() === '') return;
+
+    const li = document.createElement('li');
+    li.textContent = itemInput.value;
+    itemList.appendChild(li);
+
+    itemInput.value = '';
+}
